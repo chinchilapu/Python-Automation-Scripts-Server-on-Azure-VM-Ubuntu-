e9bd5db15ef8125966b9139b39e8e12f64a02c5d
Adharsh Chinchilapu <chinchilapu@lpu.in>
2025-07-02 15:02:29 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,18 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+from datetime import datetime
+fperson = "gmail" #The email to send from
+password = "enter password"  
+receiver = fperson #The email to send to, same as the sender in this instance
+msg = EmailMessage()
+msg[Sub] = "Daily Motivation" #Subject
+msg[From] = fperson
+msg[To] = receiver
 
+msg.set_content(fNo pain, No gain\n\nSent on: {datetime.now()}) #Set Main contents of the email
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp.gmail.com, 465, context=context) as server: #gmail server
+    server.login(fperson, password) #login
+    server.send_message(msg) #Send the message
